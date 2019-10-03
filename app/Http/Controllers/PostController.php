d765bc21daa01ef9460aec2c9041d173fd52927d
Kaspar <kasparsu@gmail.com>
2019-10-03 17:49:46 +0300

    Change posts controller and migration

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -17,15 +17,6 @@ class PostController extends Controller
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -45,17 +36,6 @@ class PostController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show(Post $post)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Post $post)
     {
         //
     }
